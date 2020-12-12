be03455b6b9be05593f080db95229202750bd036
shifatkhatoon <33038125+shifatkhatoon@users.noreply.github.com>
2020-12-12 22:19:42 +0530

    Create product.py
    create product page

---
+++ product.py
@@ -0,0 +1,13 @@
+from os import system
+from models import Product
+
+
+def show_products():
+    print("\n\n\t\tSelect Items\n\n")
+
+    print("========================================================================\n")
+    print("P.NO.\t\tNAME\t\t\t\tPRICE\t\tDiscount\n")
+    print("========================================================================\n")
+
+    for product in products:
+        print("{}\t\t{}\t\t\t{}\t\t{}%".format(product.id, product.name, str(product.price), product.discount))
