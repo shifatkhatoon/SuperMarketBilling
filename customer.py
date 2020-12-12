9679c00c4144e1b2b1b374e44d9ddadac8714f06
shifatkhatoon <33038125+shifatkhatoon@users.noreply.github.com>
2020-12-12 22:29:03 +0530

    Create customer.py

---
+++ customer.py
@@ -0,0 +1,44 @@
+from os import system
+from models import Product
+from product import show_products
+
+
+def customer_menu():
+    show_products()
+    print("\n======================================================")
+    print("\n\t\t\t\t\t\t Place Order")
+    print("\n======================================================")
+    more = 'y'
+    data = {}
+    while more == 'y':
+        p_no = int(input("\n\n Enter the P.NO. of the Item:\t"))
+        if not products.filter_by(id=p_no).first():
+            print("Enter valid product id")
+            continue
+        count = int(input("\n\n How many units? :\t"))
+        data[p_no] = count
+        more = str(input("Do You Want To Order Another Product ? (y/n)")).strip()
+        more = more
+        pass
+    print(data)
+    print("\n======================================================")
+    input("Thank you for shopping press enter to generate Invoice: ")
+    system('clear')
+    generate_invoice(data)
+    
+    
+def generate_invoice(data):
+    total = 0
+    print("\n\n********************************INVOICE************************\n")
+
+    print("\tItem\t\tQuantity\tPrice \tAmount \t\tAmount after discount\n")
+    for id in data:
+        pr = products.filter_by(id=id).first()
+        amt = data[id] * pr.price
+        damt = amt - amt * (pr.discount / 100)
+        print("\t{}\t\t{}\t{} \t{} \t\t{}".format(pr.name, data[id], pr.price, amt, damt))
+        total+=damt
+    print("\n\n\t\t\t\t\t\t\t\t\t\t Total:= Rs. ",total)
+    
+    
+    
